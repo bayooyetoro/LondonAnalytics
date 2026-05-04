3700c41c024234c28a1c46505e435b3cf51cb604
Bayo Oyetoro <oyetorojamiu7@gmail.com>
2026-05-04 11:48:30 +0100

    Committing 1 item from workspace 2538701f-1399-40bd-81a2-57d4ee5dc6d9

---
+++ 01_bronze_load_to_delta.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "44326ed2-8522-4e16-b041-da7a14617818",
+# META       "default_lakehouse_name": "london_lakehouse",
+# META       "default_lakehouse_workspace_id": "2538701f-1399-40bd-81a2-57d4ee5dc6d9",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "44326ed2-8522-4e16-b041-da7a14617818"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
